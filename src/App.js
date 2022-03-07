ca34c8ff33b79aa394f8bf44808a26b3802a9756
unknown <jacqui.longhurst@btinternet.com>
2022-03-07 22:12:55 +0100

    importing Legend component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './styles.css'
 import './App.css';
 import DonutChart from './DonutChart';
+import Legend from './Legend';
 import React from 'react';
 
 function App() {
   return (
     <div className="App">
       <DonutChart />
+      <Legend/>
     </div>
   );
 }
