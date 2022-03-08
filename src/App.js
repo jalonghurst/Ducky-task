15000f015e21b976cf53ef3cc0ba319be398c2e6
unknown <jacqui.longhurst@btinternet.com>
2022-03-08 10:14:47 +0100

    Chartjs edit cutout size, and padding

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import './styles.css'
-import './App.css';
-import DonutChart from './DonutChart';
-import Legend from './Legend';
-import React from 'react';
+import "./styles.css";
+import "./App.css";
+import DonutChart from "./DonutChart";
+import Legend from "./Legend";
+import React from "react";
 
 function App() {
   return (
     <div className="App">
       <DonutChart />
-      <Legend/>
+      <Legend />
     </div>
   );
 }
--- src/DonutChart.js
+++ src/DonutChart.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import "chart.js/auto";
 import { Doughnut } from "react-chartjs-2";
 // import { Chart, ArcElement } from "chart.js";
@@ -6,38 +6,58 @@ import { Doughnut } from "react-chartjs-2";
 const DonutChart = () => {
   return (
     <div>
-        < Doughnut 
-           data={{
-            labels: [
-                ' 25% Consumption co2mg',
-                ' 20% Energy co2mg',
-                ' 16% Food co2mg',
-                ' 26.5% Public co2mg',
-                ' 12.5% Transport co2mg',
-              ],
-              datasets: [
-                  {
-                label: 'Categories',
-                data: [2354553712.62764, 1908265269.8322, 1503715250.26774, 2497000000, 1176894355.95973],
-                backgroundColor: [
-                  '#F6BA75',
-                  '#EF5F8A',
-                  '#00A1C9',
-                  '#673E88',
-                  '#3999E3',
-                  
-                ],
+      <Doughnut
+        height="200px"
+        width="200px"
+        options={{
+          cutout: "75%",
+          layout: {
+            padding: {
+              top: 50,
+              bottom: 50,
+              left: 50,
+              right: 50,
+            },
+          },
 
-                hoverOffset: 4,
-                hoverBorderColor: '#000000',
-                hoverBorderWidth: 2,    
-                
-              },
-            ],
-           }}
-        />
+          plugins: {
+            legend: {
+              display: false,
+            },
+          },
+        }}
+        data={{
+          labels: [
+            " 25% Consumption co2mg",
+            " 20% Energy co2mg",
+            " 16% Food co2mg",
+            " 26.5% Public co2mg",
+            " 12.5% Transport co2mg",
+          ],
+          datasets: [
+            {
+              label: "Categories",
+              data: [
+                2354553712.62764, 1908265269.8322, 1503715250.26774, 2497000000,
+                1176894355.95973,
+              ],
+              backgroundColor: [
+                "#F6BA75",
+                "#EF5F8A",
+                "#00A1C9",
+                "#673E88",
+                "#3999E3",
+              ],
+
+              hoverOffset: 4,
+              hoverBorderColor: "#000000",
+              hoverBorderWidth: 2,
+            },
+          ],
+        }}
+      />
     </div>
-  )
-}
+  );
+};
 
 export default DonutChart;
