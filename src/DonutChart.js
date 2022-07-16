89bacf900e14f992fb23a1e7536a79ea6cb1a912
unknown <jacqui.longhurst@btinternet.com>
2022-07-16 15:00:29 +0200

    Changed padding to %

--- src/DonutChart.js
+++ src/DonutChart.js
@@ -13,10 +13,10 @@ const DonutChart = () => {
           cutout: "75%",
           layout: {
             padding: {
-              top: 50,
-              bottom: 50,
-              left: 50,
-              right: 50,
+              top: "5%",
+              bottom: "5%",
+              left: "5%",
+              right: "5%",
             },
           },
 
